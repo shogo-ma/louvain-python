36f660720c5a4a72e560eccfbceda8ac829723c1
shogo-ma <matsuda.shogo.mj9@is.naist.jp>
2015-11-21 23:26:15 +0900

    fix README.md and add example code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # louvain-python
+
+A implementation of Louvain method on Python.
+louvain-python implements community detection algorithm for large scale networks.
+This package uses the Louvain method described in [Fast unfolding of communities in large networks](http://arxiv.org/abs/0803.0476). Vincent D. Blondel, Jean-Loup Guillaume, Renaud Lambiotte, Etienne Lefebvre.
---
+++ example.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import networkx as nx
+
+from pylouvain import Louvain
+from collections import defaultdict
+
+def makeSampleGraph():
+    g = nx.Graph()
+    g.add_edge("a", "b", weight=1.)
+    g.add_edge("a", "c", weight=1.)
+    g.add_edge("a", "d", weight=1.)
+    g.add_edge("b", "d", weight=1.)
+    g.add_edge("c", "d", weight=1.)
+
+    g.add_edge("b", "g", weight=1.)
+
+    g.add_edge("e", "f", weight=1.)
+    g.add_edge("e", "g", weight=1.)
+    g.add_edge("e", "h", weight=1.)
+    g.add_edge("f", "h", weight=1.)
+    g.add_edge("g", "h", weight=1.)
+
+    return g
+
+if __name__ == "__main__":
+    sample_graph = makeSampleGraph()
+    partition = Louvain.getBestPartition(sample_graph)
+
+    p = defaultdict(list)
+    for node, com_id in partition.items():
+        p[com_id].append(node)
+
+    for com, nodes in p.items():
+        print(com, nodes)
