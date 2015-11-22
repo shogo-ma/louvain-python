0bd924b44ad93bd6a771b1208cece468854ea336
shogo-ma <matsuda.shogo.mj9@is.naist.jp>
2015-11-23 00:42:48 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="louvain-python",
+    version="0.0.1",
+    url="http://github.com/shogo-ma/louvain-python",
+    description="A implementation of louvain method on python",
+    author="shogo-ma",
+    packages=["louvain"],
+)
